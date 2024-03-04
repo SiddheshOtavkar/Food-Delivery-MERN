c32a39be83e0b795e8e9fdf8a18b821cdc0cd1f3
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-04 18:14:10 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { React } from 'react';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <>
+      <Toaster />
+      <div>
+        <Header />
+        <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
+          <Outlet />
+          
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
